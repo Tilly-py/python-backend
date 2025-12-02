01a579149cc10900261b5a584472c67cd52bd192
Tilly-py <alex.s.tilly@outlook.com>
2025-12-02 19:12:55 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,47 @@
 # -*- coding: utf-8 -*-
+from pydantic import BaseModel
+from unicorns import storage
+from fastapi import FastAPI, Header
 
-from fastapi import FastAPI
+class SpottedWhere(BaseModel):
+    location: str
+    lat: float
+    lon: float
+
+class Unicorns(BaseModel):
+    id: int
+    name: str
+    description: str
+    reportedBy: str
+    spottedWhere: SpottedWhere
+    spottedWhen: str
+    image: str
 
 app = FastAPI()
 
 @app.get("/")
 async def hello():
     return {"Hello": "World!"}
+
+@app.get("/unicorns")
+async def get_unicorns(accept: str = Header(default="application/json")):
+    print(f"Accept header: {accept}")
+    unicorns = storage.fetch_unicorns()
+    return unicorns
+
+
+@app.get("/unicorn/{unicorn_id}")
+async def get_unicorn(unicorn_id: int):
+    pass
+
+@app.post("/unicorn")
+async def create_unicorn():
+    pass
+
+@app.put("/unicorn/{unicorn_id}")
+async def update_unicorn(unicorn_id: int):
+    pass
+
+@app.delete("/unicorn/{unicorn_id}")
+async def delete_unicorn(unicorn_id: int):
+    pass
