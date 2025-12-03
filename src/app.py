76117aa3b88950bbdf9e9731e25e2768e20f616e
Tilly-py <alex.s.tilly@outlook.com>
2025-12-03 14:09:23 +0100

    Nukörvi
    lol

--- src/app.py
+++ src/app.py
@@ -1,47 +1,65 @@
 # -*- coding: utf-8 -*-
 from pydantic import BaseModel
-from unicorns import storage
+from unicorns import storage , Unicorn
 from fastapi import FastAPI, Header
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
-class SpottedWhere(BaseModel):
-    location: str
-    lat: float
-    lon: float
 
-class Unicorns(BaseModel):
-    id: int
-    name: str
-    description: str
-    reportedBy: str
-    spottedWhere: SpottedWhere
-    spottedWhen: str
-    image: str
+# class SpottedWhere(BaseModel):
+#     name: str
+#     lat: float
+#     lon: float
+
+# class Unicorn(BaseModel):
+#     name: str
+#     description: str
+#     reportedBy: str
+#     spottedWhere: SpottedWhere
+#     spottedWhen: str
+#     image: str
 
 app = FastAPI()
 
-@app.get("/")
-async def hello():
-    return {"Hello": "World!"}
+app.add_middleware(
+CORSMiddleware,
+allow_origins=["*"],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+)
 
-@app.get("/unicorns")
+templates = Jinja2Templates(directory="src/templates")
+
+
+
+@app.get("/")
 async def get_unicorns(accept: str = Header(default="application/json")):
     print(f"Accept header: {accept}")
     unicorns = storage.fetch_unicorns()
     return unicorns
 
 
-@app.get("/unicorn/{unicorn_id}")
-async def get_unicorn(unicorn_id: int):
-    pass
+@app.get("/{unicorn_id}")
+async def get_unicorn(unicorn_id: int, accept: str = Header(default="application/json")):
+    print(f"Accept header: {accept}")
+    unicorn = storage.fetch_unicorn(unicorn_id)
+    return unicorn
 
-@app.post("/unicorn")
-async def create_unicorn():
-    pass
+@app.post("/")
+async def create_unicorn(unicorn: Unicorn):
+    storage.add_unicorn(unicorn)
+    return {"message": "Unicorn created successfully", "unicorn": unicorn}
 
-@app.put("/unicorn/{unicorn_id}")
+@app.put("/{unicorn_id}")
 async def update_unicorn(unicorn_id: int):
-    pass
+    unicorn = storage.fetch_unicorn(unicorn_id)
+    storage.update_unicorn(unicorn)
+    return {"message": "ok"}
 
-@app.delete("/unicorn/{unicorn_id}")
+
+@app.delete("/{unicorn_id}")
 async def delete_unicorn(unicorn_id: int):
-    pass
+    storage.delete_unicorn(unicorn_id)
+    return {"message": "Unicorn deleted successfully"}
--- src/unicorns/Unicorn.py
+++ src/unicorns/Unicorn.py
@@ -9,51 +9,51 @@ class Unicorn(BaseModel):
     '''
     En enkel klass för att representera en enhörning
     '''
-    
+
     id: int = 0
     name: str = ""
     description: str = ""
-    reported_by: str = ""
-    spotted_where: Location  =  Location()
-    spotted_when: str | None = None
+    reportedBy: str = ""
+    spottedWhere: Location  =  Location()
+    spottedWhen: str | None = None
     image: str = ""
-    
+
     def from_db(self, data: list) -> None:
         '''
         Populerar en enhörning med data från en databasförfrågan.
         '''
-        
+
         self.id = data[0]
         self.name = data[1]
         self.description = data[2]
-        self.reported_by = data[3]
-        self.spotted_where.name = data[4]
-        self.spotted_where.lat = data[5]
-        self.spotted_where.lon = data[6]
-        self.spotted_when = data[7]
+        self.reportedBy = data[3]
+        self.spottedWhere.name = data[4]
+        self.spottedWhere.lat = data[5]
+        self.spottedWhere.lon = data[6]
+        self.spottedWhen = data[7]
         self.image = data[8]
-    
+
     def to_dict(self, nested = False):
         '''
         Skapar en dictionary med värden från denna enhörning. Bra att använda
         när man matar in enhörningar i databaser.
-        
+
         Med en liten fix kan vi göra den här lämplig även för JSON-representation.
         '''
-        
+
         if nested:
             location = {
-                'name': self.spotted_where.name,
-                'lat': self.spotted_where.lat,
-                'lon': self.spotted_where.lon
+                'name': self.spottedWhere.name,
+                'lat': self.spottedWhere.lat,
+                'lon': self.spottedWhere.lon
             }
             unicorn =  {
                 'id': self.id,
                 'name': self.name,
                 'description': self.description,
-                'reportedBy': self.reported_by,
+                'reportedBy': self.reportedBy,
                 'spottedWhere': location,
-                'spottedWhen': self.spotted_when,
+                'spottedWhen': self.spottedWhen,
                 'image': self.image
             }
         else:
@@ -61,12 +61,12 @@ class Unicorn(BaseModel):
                     'id': self.id,
                     'name': self.name,
                     'description': self.description,
-                    'reportedBy': self.reported_by,
-                    'spottedWhereName': self.spotted_where.name,
-                    'spottedWhereLat': self.spotted_where.lat,
-                    'spottedWhereLon': self.spotted_where.lon,
-                    'spottedWhen': self.spotted_when,
+                    'reportedBy': self.reportedBy,
+                    'spottedWhereName': self.spottedWhere.name,
+                    'spottedWhereLat': self.spottedWhere.lat,
+                    'spottedWhereLon': self.spottedWhere.lon,
+                    'spottedWhen': self.spottedWhen,
                     'image': self.image
                 }
-                
+
         return unicorn
